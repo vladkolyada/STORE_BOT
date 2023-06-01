7679e5ada74cfe9fc277173509e9ecded9944776
vladkolyada <v.kolyada7.13@gmail.com>
2023-06-01 20:53:25 +0300

    changed callback_datas

--- keyboards/inline/default_keyboard.py
+++ keyboards/inline/default_keyboard.py
@@ -3,8 +3,8 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 fruits_or_vegetables = InlineKeyboardMarkup(inline_keyboard=[
     [
-        InlineKeyboardButton('Fruits 🍇', callback_data='fruits'),
-        InlineKeyboardButton('Vegetables 🥕', callback_data='vegetables')
+        InlineKeyboardButton('Fruits 🍇', callback_data='/fruits'),
+        InlineKeyboardButton('Vegetables 🥕', callback_data='/vegetables')
     ]
 ])
 
