900cdfbe7e3065488557305ed2f8ad7c2e8fa446
vladkolyada <v.kolyada7.13@gmail.com>
2023-06-21 14:37:02 +0300

    added new changes

--- handlers/users/callbacks.py
+++ handlers/users/callbacks.py
@@ -3,11 +3,11 @@ from aiogram import types
 
 from loader import dp
 from states.steps import StatesForBot
-from keyboards.inline.fruits.fruit_selection import types_of_fruits, sorts_of_apple, back_to_sorts_of_apple_or_buy_gd, \
-    back_to_sorts_of_apple_or_buy_idared, sorts_of_watermelon, back_to_sorts_of_watermelon_or_buy_arsenal, \
-    back_to_sorts_of_watermelon_or_buy_sensei, sorts_of_grape, back_to_sorts_of_grape_or_buy_bianca, \
-    back_to_sorts_of_grape_or_buy_olymp, sorts_of_banana, back_to_sorts_of_banana_or_buy_cavendish, \
-    back_to_sorts_of_banana_or_buy_morado
+from keyboards.inline.fruits.fruit_selection import types_of_fruits, types_of_apples, back_to_types_of_apples_or_buy_gd, \
+    back_to_types_of_apples_or_buy_idared, types_of_watermelons, back_to_types_of_watermelons_or_buy_arsenal, \
+    back_to_types_of_watermelons_or_buy_sensei, types_of_grapes, back_to_types_of_grapes_or_buy_bianca, \
+    back_to_types_of_grapes_or_buy_olymp, types_of_bananas, back_to_types_of_bananas_or_buy_cavendish, \
+    back_to_types_of_bananas_or_buy_morado
 from keyboards.inline.vegetables.vegetable_selection import types_of_vegetables
 from keyboards.inline.default_keyboard import fruits_or_vegetables
 
@@ -25,70 +25,79 @@ async def fruit_or_vegetables(callback: types.CallbackQuery):
                                       reply_markup=types_of_vegetables)
 
     # fruits
+    # only for apple
+    # different sorts of apple
     elif callback.data == '/fruits/apple':
         await callback.message.delete()
         await callback.message.answer(text='Choose apple sort.',
-                                      reply_markup=sorts_of_apple)
+                                      reply_markup=types_of_apples)
     elif callback.data == '/fruits/apple/golden_delicious':
         await callback.message.delete()
-        await callback.message.answer_photo(photo=open(str(os.path.abspath('golden_delicious.jpg')), 'rb'),
+        await callback.message.answer_photo(photo=open(str(os.path.abspath('golden_delicious_apple.jpg')), 'rb'),
                                             caption='Apple sort - Golden Delicious\n'
                                                     'Price per kilo: $1.63',
-                                            reply_markup=back_to_sorts_of_apple_or_buy_gd)
+                                            reply_markup=back_to_types_of_apples_or_buy_gd)
     elif callback.data == '/fruits/apple/idared':
         await callback.message.delete()
-        await callback.message.answer_photo(photo=open(str(os.path.abspath('idared.jpg')), 'rb'),
+        await callback.message.answer_photo(photo=open(str(os.path.abspath('idared_apple.jpg')), 'rb'),
                                             caption='Apple sort - Idared\n'
                                                     'Price per kilo: $1.00',
-                                            reply_markup=back_to_sorts_of_apple_or_buy_idared)
+                                            reply_markup=back_to_types_of_apples_or_buy_idared)
+    # only for watermelon
+    # different sorts of watermelon
     elif callback.data == '/fruits/watermelon':
         await callback.message.delete()
-        await callback.message.answer(text='Choose apple sort.',
-                                      reply_markup=sorts_of_watermelon)
+        await callback.message.answer(text='Choose watermelon sort.',
+                                      reply_markup=types_of_watermelons)
     elif callback.data == '/fruits/watermelon/arsenal':
         await callback.message.delete()
-        await callback.message.answer_photo(photo=open(str(os.path.abspath('arsenal.jpg')), 'rb'),
+        await callback.message.answer_photo(photo=open(str(os.path.abspath('arsenal_watermelon.jpg')), 'rb'),
                                             caption='Watermelon sort - Arsenal\n'
                                                     'Price per kilo: $0.55',
-                                            reply_markup=back_to_sorts_of_watermelon_or_buy_arsenal)
+                                            reply_markup=back_to_types_of_watermelons_or_buy_arsenal)
     elif callback.data == '/fruits/watermelon/sensei':
         await callback.message.delete()
-        await callback.message.answer_photo(photo=open(str(os.path.abspath('sensei.jpg')), 'rb'),
+        await callback.message.answer_photo(photo=open(str(os.path.abspath('sensei_watermelon.jpg')), 'rb'),
                                             caption='Watermelon sort - Sensei\n'
                                                     'Price per kilo: $0.60',
-                                            reply_markup=back_to_sorts_of_watermelon_or_buy_sensei)
+                                            reply_markup=back_to_types_of_watermelons_or_buy_sensei)
+    # only for grape
+    # different sorts of grape
     elif callback.data == '/fruits/grape':
         await callback.message.delete()
         await callback.message.answer(text='Choose grape sort',
-                                      reply_markup=sorts_of_grape)
+                                      reply_markup=types_of_grapes)
     elif callback.data == '/fruits/grape/olymp':
         await callback.message.delete()
-        await callback.message.answer_photo(photo=open(str(os.path.abspath('olymp.jpg')), 'rb'),
+        await callback.message.answer_photo(photo=open(str(os.path.abspath('olymp_grape.jpg')), 'rb'),
                                             caption='Grape sort - Olymp\n'
                                                     'Price per kilo: $2.35',
-                                            reply_markup=back_to_sorts_of_grape_or_buy_olymp)
+                                            reply_markup=back_to_types_of_grapes_or_buy_olymp)
     elif callback.data == '/fruits/grape/bianca':
         await callback.message.delete()
-        await callback.message.answer_photo(photo=open(str(os.path.abspath('bianca.jpg')), 'rb'),
+        await callback.message.answer_photo(photo=open(str(os.path.abspath('bianca_grape.jpg')), 'rb'),
                                             caption='Grape sort - Bianca\n'
                                                     'Price per kilo: $2.10',
-                                            reply_markup=back_to_sorts_of_grape_or_buy_bianca)
+                                            reply_markup=back_to_types_of_grapes_or_buy_bianca)
+    # only for banana
     elif callback.data == '/fruits/banana':
         await callback.message.delete()
-        await callback.message.answer(text='Choose grape sort',
-                                      reply_markup=sorts_of_banana)
+        await callback.message.answer(text='Choose banana sort',
+                                      reply_markup=types_of_bananas)
+    # different sorts of banana
     elif callback.data == '/fruits/banana/cavendish':
         await callback.message.delete()
-        await callback.message.answer_photo(photo=open(str(os.path.abspath('cavendish.jpg')), 'rb'),
+        await callback.message.answer_photo(photo=open(str(os.path.abspath('cavendish_banana.jpg')), 'rb'),
                                             caption='Banana sort - Cavendish\n'
                                                     'Price per kilo: $2.95',
-                                            reply_markup=back_to_sorts_of_banana_or_buy_cavendish)
+                                            reply_markup=back_to_types_of_bananas_or_buy_cavendish)
     elif callback.data == '/fruits/banana/morado':
         await callback.message.delete()
-        await callback.message.answer_photo(photo=open(str(os.path.abspath('morado.jpg')), 'rb'),
+        await callback.message.answer_photo(photo=open(str(os.path.abspath('morado_banana.jpg')), 'rb'),
                                             caption='Banana sort - Morado\n'
                                                     'Price per kilo: $3.20',
-                                            reply_markup=back_to_sorts_of_banana_or_buy_morado)
+                                            reply_markup=back_to_types_of_bananas_or_buy_morado)
+    # back to selection between fruits and vegetables
     elif callback.data == '/back':
         await callback.message.delete()
         await callback.message.answer(text='Choose type of product.',
--- keyboards/inline/vegetables/vegetable_selection.py
+++ keyboards/inline/vegetables/vegetable_selection.py
@@ -3,19 +3,80 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 types_of_vegetables = InlineKeyboardMarkup(inline_keyboard=[
     [
-        InlineKeyboardButton('Potato 🥔', callback_data='/vegetable/apple')
+        InlineKeyboardButton('Potato 🥔', callback_data='/vegetable/potato')
     ],
     [
-        InlineKeyboardButton('Tomato 🍅', callback_data='/vegetable/watermelon')
+        InlineKeyboardButton('Tomato 🍅', callback_data='/vegetable/tomato')
     ],
     [
-        InlineKeyboardButton('Corn 🌽', callback_data='/vegetable/grape')
+        InlineKeyboardButton('Corn 🌽', callback_data='/vegetable/corn')
     ],
     [
-        InlineKeyboardButton('Carrot 🥕', callback_data='/vegetable/banana')
+        InlineKeyboardButton('Carrot 🥕', callback_data='/vegetable/carrot')
     ],
     [
         InlineKeyboardButton('Back', callback_data='/back')
     ]
 ])
 
+
+types_of_potatoes = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton('Russet', callback_data='/vegetables/potato/russet')
+    ],
+    [
+        InlineKeyboardButton('Kennebec', callback_data='/vegetables/potato/kennebec')
+    ],
+    [
+        InlineKeyboardButton('Back', callback_data='/vegetables')
+    ]
+])
+
+back_to_types_of_potatoes_or_buy_russet = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton('Buy', callback_data='/vegetables/potato/russet/buy_russet')
+    ],
+    [
+        InlineKeyboardButton('Back', callback_data='/vegetables/potato')
+    ]
+])
+
+back_to_types_of_potatoes_or_buy_kennebec = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton('Buy', callback_data='/vegetables/potato/kennebec/buy_kennebec')
+    ],
+    [
+        InlineKeyboardButton('Back', callback_data='/vegetables/potato')
+    ]
+])
+
+
+types_of_tomatoes = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton('Alicante', callback_data='/vegetables/tomato/alicante')
+    ],
+    [
+        InlineKeyboardButton('Beefsteak', callback_data='/vegetables/tomato/beefsteak')
+    ],
+    [
+        InlineKeyboardButton('Back', callback_data='/vegetables')
+    ]
+])
+
+back_to_types_of_tomatoes_or_buy_alicante = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton('Buy', callback_data='/vegetables/tomato/alicante/buy_alicante')
+    ],
+    [
+        InlineKeyboardButton('Back', callback_data='/vegetables/tomatoes')
+    ]
+])
+
+back_to_types_of_tomatoes_or_buy_beefsteak = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton('Buy', callback_data='/vegetables/tomato/beefsteak/buy_beefsteak')
+    ],
+    [
+        InlineKeyboardButton('Back', callback_data='/vegetables/tomatoes')
+    ]
+])
