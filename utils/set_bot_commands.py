e8322c99f5faa915b4d96712cca669e167a2c250
vladkolyada <v.kolyada7.13@gmail.com>
2023-06-28 21:18:21 +0300

    /send_order and /check_data commands have been removed

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,9 +5,6 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand('start', 'Start the bot'),
-            types.BotCommand('help', "Get all commands"),
-            types.BotCommand('basket', "Check data of basket"
-                                       "(works only after passing authorization by entering "
-                                       "the /start command)")
+            types.BotCommand('help', "Get all commands")
         ]
     )
