92f3f7982835dccd096a1b9e085d330acd894fcc
vladkolyada <v.kolyada7.13@gmail.com>
2023-06-28 21:14:09 +0300

    added new really cool keyboard

--- keyboards/default/default_keyboards.py
+++ keyboards/default/default_keyboards.py
@@ -8,3 +8,15 @@ get_contact = ReplyKeyboardMarkup(keyboard=[
 ], resize_keyboard=True)
 
 remove_kb = ReplyKeyboardRemove()
+
+
+def kb_for_basket(basket: list):
+    kb_basket = [[KeyboardButton('Clear all from basket')]]
+    for i in basket:
+        kb_basket.append(
+            [
+                KeyboardButton(f'Delete {i}')
+            ]
+        )
+
+    return ReplyKeyboardMarkup(keyboard=kb_basket)
