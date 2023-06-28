0ace39e856a039c5da95efc619120537e1fb64ba
vladkolyada <v.kolyada7.13@gmail.com>
2023-06-28 21:12:34 +0300

    added new changes

--- handlers/users/get_contact.py
+++ handlers/users/get_contact.py
@@ -1,6 +1,7 @@
 from aiogram import types
 
 from loader import dp, db
+from keyboards.default.default_keyboards import remove_kb
 from keyboards.inline.default_keyboard import fruits_or_vegetables
 from states.steps import StatesForBot
 
@@ -8,7 +9,8 @@ from states.steps import StatesForBot
 @dp.message_handler(content_types=types.ContentType.CONTACT)
 async def getting_the_phone_number(message: types.Message):
     await StatesForBot.AfterAuthorization.set()
-    await db.add_new_user_to_the_table(message.contact.user_id, message.contact.phone_number)
-    await message.answer(text='Thanks for that!')
+    await db.add_new_user_to_the_table(id=message.contact.user_id, phone_number=message.contact.phone_number,
+                                       first_name=message.contact.first_name, last_name=message.contact.last_name)
+    await message.answer(text='Thanks for that!', reply_markup=remove_kb)
     await message.answer(text='Go shopping! Choose type of product.',
                          reply_markup=fruits_or_vegetables)
